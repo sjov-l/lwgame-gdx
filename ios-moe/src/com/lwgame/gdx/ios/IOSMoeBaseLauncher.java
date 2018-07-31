9de6406117731c0227c96df40cd0e50806a08b44
bbflys@126.com <bbflys@126.com>
2018-07-31 10:28:33 +0800

    *ios language initialize

--- ios-moe/src/com/lwgame/gdx/ios/IOSMoeBaseLauncher.java
+++ ios-moe/src/com/lwgame/gdx/ios/IOSMoeBaseLauncher.java
@@ -8,12 +8,15 @@ import com.lwgame.gdx.Lw;
 import com.lwgame.gdx.conf.Configuration;
 import com.lwgame.gdx.ios.ads.IOSAdmob;
 import com.lwgame.gdx.ios.billing.IOSBilling;
+import com.lwgame.gdx.ios.bindings.FIRApp;
 import com.lwgame.gdx.ios.platform.IOSGameCenter;
 import com.lwgame.gdx.ios.platform.IOSPlatform;
 
 import org.moe.natj.general.Pointer;
 
 import apple.foundation.NSDictionary;
+import apple.foundation.NSLocale;
+import apple.foundation.c.Foundation;
 import apple.uikit.UIApplication;
 import apple.uikit.c.UIKit;
 
@@ -25,6 +28,7 @@ public abstract class IOSMoeBaseLauncher extends IOSApplication.Delegate {
 
     @Override
     protected IOSApplication createApplication() {
+        initDefaultLocale();
         IOSApplicationConfiguration config = new IOSApplicationConfiguration();
         config.useAccelerometer = false;
         return new IOSApplication(createApplicationListener(), config);
@@ -33,6 +37,7 @@ public abstract class IOSMoeBaseLauncher extends IOSApplication.Delegate {
     @Override
     public boolean applicationDidFinishLaunchingWithOptions(UIApplication application, NSDictionary<?, ?> launchOptions) {
         boolean r = super.applicationDidFinishLaunchingWithOptions(application, launchOptions);
+        FIRApp.configure();
         IOSApplication app = (IOSApplication) Gdx.app;
         Lw.configuration = new Configuration();
         Lw.billing = new IOSBilling();
@@ -40,6 +45,14 @@ public abstract class IOSMoeBaseLauncher extends IOSApplication.Delegate {
         Lw.platform = new IOSPlatform();
         Lw.ads = new IOSAdmob(app);
         return r;
+    }
+
+    protected void initDefaultLocale() {
+        NSLocale nsLocale = NSLocale.autoupdatingCurrentLocale();
+        String language = nsLocale.objectForKey(Foundation.NSLocaleLanguageCode()).toString();
+        String country = nsLocale.objectForKey(Foundation.NSLocaleCountryCode()).toString();
+        System.setProperty("user.language", language);
+        System.setProperty("user.region", country);
     }
 
     protected abstract ApplicationListener createApplicationListener();
